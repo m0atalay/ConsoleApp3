8f9726a2df8a49fea21a9db379d163dfca527dcb
Melik Şah ATALAY <meliksah.atalay@gmail.com>
2024-01-19 15:15:25 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+     class Class1
+    {
+        public void Topla(int sayi1,  int sayi2)
+        {
+          int  toplam = sayi1 + sayi2;
+        }
+        public void Cıkarma(int sayi1, int sayi2)
+        {
+            int cıkarma = sayi1 - sayi2;
+        }
+        public void Bolme(int sayi1, int sayi2)
+        {
+            int bolme = sayi1 / sayi2;
+        }
+        public void Carpma(int sayi1, int sayi2)
+        {
+            int toplam = sayi1 * sayi2;
+            Console.WriteLine("Toplam Sonuç = " + toplam);
+        }
+
+        public string Isım { get; set; }
+        public string Soyad { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp3;
+
+Class1 dort = new Class1();
+
+
+dort.Carpma(6, 5);
+
+Console.WriteLine(dort);
+
+
+
+Class1 Sayi = new Class1();
+
+Sayi.Isım = "Melikşah Atalay";
+Sayi.Age = 5;
+Sayi.Soyad = "Atalay";
+
+
+Class1 Sayi1 = new Class1();
+
+Sayi1.Isım = "Melikşah sad";
+Sayi1.Age = 45;
+Sayi1.Soyad = "das";
+
+Class1[] my = new Class1[] { Sayi, Sayi1 };
+
+foreach (var my1 in my)     
+{
+    Console.WriteLine(my1.Isım);
+}
+
+
